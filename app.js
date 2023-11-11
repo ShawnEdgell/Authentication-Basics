f17b3f2001e74524b8626a1aeaa86112b919382d
shawn edgell <shawn_edgell@hotmail.com>
2023-11-10 23:25:23 -0500

    bascreate user, login/logout

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// MongoDB Connection
+const mongoDb = "mongodb+srv://admin:secretadminpassword@cluster2.vj9c7dk.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+// User Schema
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Express Session Setup
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+// Passport Configuration
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" });
+    };
+    if (user.password !== password) {
+      return done(null, false, { message: "Incorrect password" });
+    };
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  };
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Custom middleware to make user info available in templates
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    await user.save();
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/log-in", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/"
+}));
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) { return next(err); }
+    res.redirect("/");
+  });
+});
+
+// Starting the Server
+app.listen(3000, () => console.log("app listening on port 3000!"));
